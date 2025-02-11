844e0fdf89b0f40622088792ea64e4d3a54e6bc4
4lvaro18 <a.mota.2023@alumnos.urjc.es>
2025-02-11 16:53:26 +0100

    java1

---
+++ JobMatcherApplication.java
@@ -0,0 +1,11 @@
+package com.tuapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobMatcherApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JobMatcherApplication.class, args);
+    }
+}
