54acf9ee92a2ca9495d9f3f97d9236aca8f74de8
CarlosMarronBenito <160424096+CarlosMarronBenito@users.noreply.github.com>
2025-02-11 18:54:59 +0100

    spring

---
+++ src/main/java/es/grupo18/jobmatcher/JobmatcherApplication.java
@@ -0,0 +1,13 @@
+package es.grupo18.jobmatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobmatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobmatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/grupo18/jobmatcher/JobmatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package es.grupo18.jobmatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobmatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
