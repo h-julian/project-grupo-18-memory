89d44f1c974d66e13d820f16c10f68c9c5917cb6
h-julian <h.julian.2023@alumnos.urjc.es>
2025-02-13 20:26:50 +0100

    Error Error

--- src/main/java/es/grupo18/jobmatcher/controller/CustomErrorController.java
+++ src/main/java/es/grupo18/jobmatcher/controller/CustomErrorController.java
@@ -1,6 +1,5 @@
 package es.grupo18.jobmatcher.controller;
 
-
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -12,25 +11,27 @@ import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
-@RequestMapping("/error")
 public class CustomErrorController implements ErrorController {
 
-    @GetMapping
+    @GetMapping("/error")
     public String handleError(HttpServletRequest request, Model model) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-        
-        if (status != null) {
-            int statusCode = Integer.parseInt(status.toString());
-            model.addAttribute("status", statusCode);
 
-            if (statusCode == HttpStatus.NOT_FOUND.value()) {
-                model.addAttribute("error", "Página no encontrada");
-            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-                model.addAttribute("error", "Error interno del servidor");
-            } else {
-                model.addAttribute("error", "Error inesperado");
-            }
+        int statusCode = (status != null) ? Integer.parseInt(status.toString()) : 500;
+        model.addAttribute("status", statusCode);
+
+        if (statusCode == HttpStatus.NOT_FOUND.value()) {
+            model.addAttribute("error", "Página no encontrada");
+        } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+            model.addAttribute("error", "Error interno del servidor");
+        } else {
+            model.addAttribute("error", "Error inesperado");
         }
-        return "error"; // Carga `error.mustache`
+
+        return "error";
+    }
+
+    public String getErrorPath() {
+        return "/error";
     }
 }
