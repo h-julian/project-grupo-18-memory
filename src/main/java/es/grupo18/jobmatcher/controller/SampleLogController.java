5ce5feb41944248edd1ff5b2b118b1515aa717cd
h-julian <h.julian.2023@alumnos.urjc.es>
2025-02-17 10:17:26 +0100

    Simplified
    Favicon - ADD
    Error - OK
    getErrorPath() - DELETE

--- src/main/java/es/grupo18/jobmatcher/controller/SampleLogController.java
+++ src/main/java/es/grupo18/jobmatcher/controller/SampleLogController.java
@@ -2,22 +2,26 @@ package es.grupo18.jobmatcher.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 
-@Controller
+
+
+@RestController
 public class SampleLogController {
- private Logger log = LoggerFactory.getLogger(SampleLogController.class);
- @GetMapping("/page_log")
- public String page(Model model) {
+ private static final Logger log = LoggerFactory.getLogger(SampleLogController.class);
+ @GetMapping("/log_page")
+ public ResponseEntity<Void> logMessages() {
    log.trace("A TRACE Message");
    log.debug("A DEBUG Message");
    log.info("An INFO Message");
    log.warn("A WARN Message");
    log.error("An ERROR Message");
-   return "page";
+
+  return ResponseEntity.status(HttpStatus.NO_CONTENT).build(); // Devuelve 204 No Content
  }
 }
