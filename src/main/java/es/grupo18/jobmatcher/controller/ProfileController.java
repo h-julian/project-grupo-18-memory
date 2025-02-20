5a2ebcded4b266c8697b81466b2e785d6c13b930
CarlosMarronBenito <160424096+CarlosMarronBenito@users.noreply.github.com>
2025-02-20 14:36:26 +0100

    Update ProfileController.java

--- src/main/java/es/grupo18/jobmatcher/controller/ProfileController.java
+++ src/main/java/es/grupo18/jobmatcher/controller/ProfileController.java
@@ -1,13 +1,13 @@
 package es.grupo18.jobmatcher.controller;
 
-import es.grupo18.jobmatcher.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpSession;
+import es.grupo18.jobmatcher.model.User;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class ProfileController {
