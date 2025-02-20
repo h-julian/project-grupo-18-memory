7cf147f13932e50027ad3d77556d26841edfd6ed
dmartinm2023 <d.martinm.2023@alumnos.urjc.es>
2025-02-20 12:26:59 +0100

    Fixes

--- src/main/java/es/grupo18/jobmatcher/repository/UserRepository.java
+++ src/main/java/es/grupo18/jobmatcher/repository/UserRepository.java
@@ -1,9 +1,8 @@
 package es.grupo18.jobmatcher.repository;
 
-import es.grupo18.jobmatcher.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository {
 }
