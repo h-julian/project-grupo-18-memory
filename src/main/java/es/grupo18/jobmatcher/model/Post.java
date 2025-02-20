e91c2de8b87e92ea26a8ff8b01f07f88276c0790
dmartinm2023 <d.martinm.2023@alumnos.urjc.es>
2025-02-20 20:37:08 +0100

    System to create new blog posts

--- src/main/java/es/grupo18/jobmatcher/model/Post.java
+++ src/main/java/es/grupo18/jobmatcher/model/Post.java
@@ -1,13 +1,11 @@
 package es.grupo18.jobmatcher.model;
 
-import java.util.Date;
-
 public class Post {
     
     private long postId; 
     private String title;
     private String content;
-    private Date timestamp; 
+    private String timestamp; 
     private String imagePath;
     private long ownerId;
     private String ownerType;
@@ -15,11 +13,11 @@ public class Post {
     // Constructor
     public Post() {}
 
-    public Post(long postId, String title, String content, Date timestamp, String imagePath, long ownerId, String ownerType) {
+    public Post(long postId, String title, String content, String timestamp, String imagePath, long ownerId, String ownerType) {
         this.postId = postId;
         this.title = title;
         this.content = content;
-        this.timestamp = (timestamp != null) ? timestamp : new Date();
+        this.timestamp = timestamp;
         this.imagePath = imagePath;
         this.ownerId = ownerId;
         this.ownerType = ownerType;
@@ -29,7 +27,7 @@ public class Post {
     public long getPostId() { return postId; }
     public String getTitle() { return title; }
     public String getContent() { return content; }
-    public Date getTimestamp() { return timestamp; }
+    public String getTimestamp() { return timestamp; }
     public String getImagePath() { return imagePath; }
     public long getOwnerId() { return ownerId; }
     public String getOwnerType() { return ownerType; }
@@ -38,10 +36,9 @@ public class Post {
     public void setPostId(long postId) { this.postId = postId; }
     public void setTitle(String title) { this.title = title; }
     public void setContent(String content) { this.content = content; }
-    public void setTimestamp(Date timestamp) { this.timestamp = timestamp; }
+    public void setTimestamp(String timestamp) { this.timestamp = timestamp; }
     public void setImagePath(String imagePath) { this.imagePath = imagePath; }
     public void setOwnerId(long ownerId) { this.ownerId = ownerId; }
     public void setOwnerType(String ownerType) { this.ownerType = ownerType; }
 
 }
-
